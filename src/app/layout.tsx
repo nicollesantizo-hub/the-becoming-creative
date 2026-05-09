d63f6ea1c8946cc5a671e4777c4f3401af57ee60
Aida Visuals <aida@aidavisuals.com>
2026-05-09 00:58:58 -0700

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk, Caveat, DM_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const heading = Space_Grotesk({
@@ -39,7 +40,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${heading.variable} ${accent.variable} ${body.variable} h-full`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Script src="/_vercel/insights/script.js" strategy="afterInteractive" />
+      </body>
     </html>
   );
 }
